e3046e20c8965ec37036e43e1c5b0ee826fee1fa
LUIZA LIMA DA ROSA <60203049063@senacrs.edu.br>
2026-07-06 17:26:24 -0300

    salvar navbar.js

---
+++ app/script/navbar.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    
+    const token = localStorage.getItem('authToken');
+    
+    
+    const links = document.querySelectorAll('.link-do-cabecalho');
+
+    links.forEach(link => {
+        
+        if (link.getAttribute('href') === './auth.html') {
+            
+            
+            if (token) {
+                link.setAttribute('href', './form.html');
+            }
+        }
+    })
+})
